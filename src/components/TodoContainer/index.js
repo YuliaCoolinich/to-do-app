be2e0429f271687918802d14d06ba83fa2158c24
YuliaCoolinich <64796608+YuliaCoolinich@users.noreply.github.com>
2024-03-07 22:08:24 +0200

    Add Todo container on App page

---
+++ src/components/TodoContainer/index.js
@@ -0,0 +1,15 @@
+import InputTaskPanel from '../InputTaskPanel'
+import SortingPanel from '../SortingPanel'
+import TasksList from '../TasksList'
+
+const TodoContainer = () => {
+  return (
+   <>
+     <InputTaskPanel />
+     <SortingPanel />
+     <TasksList />
+   </>
+  )
+}
+
+export default TodoContainer
