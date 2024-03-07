6d58e59ad71db187f12bae597c47d956f2990ac0
YuliaCoolinich <64796608+YuliaCoolinich@users.noreply.github.com>
2024-03-08 01:02:03 +0200

    Add cursor at text task

--- src/components/base-components/Typography/index.js
+++ src/components/base-components/Typography/index.js
@@ -2,10 +2,11 @@ import { Typography } from '@mui/material/'
 import { styled } from '@mui/material/styles'
 import { deepPurple } from '@mui/material/colors'
 
-export default styled(Typography)(({ color, textAlign, width }) => ({
+export default styled(Typography)(({ color, textAlign, width, cursor }) => ({
   'color': color ?? deepPurple[900],
   'overflowWrap': 'break-word',
   'width': width ?? '100%',
   'textAlign': textAlign ?? 'start',
   'padding': '10px',
+  'cursor': cursor ? 'pointer' : 'text',
 }))
